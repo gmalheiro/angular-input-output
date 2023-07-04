34a2fc71acc386163dc56fbb6e5f7016ac693eae
gabriel malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-03 22:10:32 -0300

    Cockpit component initial structure done

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent {
-
+  serverName:string = '';
+  serverContent:string = '';
 }
